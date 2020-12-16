408d61c4ba0fe34ebc9ddac0aa652ec5503a9b52
dpipi17 <dpipi17@freeuni.edu.ge>
2020-12-16 23:52:13 +0400

    add some details and make games page responsive

--- Games Page/Custom Elements/gameCard.js
+++ Games Page/Custom Elements/gameCard.js
@@ -1,26 +1,40 @@
 import TeamsPage from './../../Teams page/teams.js'
 
 export default class GameCard extends HTMLElement {
+
+    static renderCardMiddle(game) {
+        if (game.gameDuration.hours === '') {
+            return `
+                <h2>${game.startTimeEastern}</h2>
+            `;
+        }
+
+        return `
+            <h1>${game.vTeam.score} </h1>    
+            <h3>Final</h3>
+            <h1>${game.hTeam.score}</h1>           
+        `;
+    }
+
     connectedCallback() {
         var game = JSON.parse(decodeURIComponent(this.getAttribute('data')));
-        console.log(game);
         this.innerHTML = `
             <a class="gamecard" href="#/Game/${game.gameId}">
-                <div class="team_in_card">
-                    <img src=${TeamsPage.photo_links[game.hTeam.triCode]} alt="">
-                    <h3>${game.hTeam.triCode}</h3>
-                    <p>${game.hTeam.win}-${game.hTeam.loss}</p>
-                </div>
-
-                ${
-                    'vs'
-                }
-
                 <div class="team_in_card">
                     <img src=${TeamsPage.photo_links[game.vTeam.triCode]} alt="">
                     <h3>${game.vTeam.triCode}</h3>
                     <p>${game.vTeam.win}-${game.vTeam.loss}</p>
                 </div>
+
+                <div class="cardMiddle">
+                    ${GameCard.renderCardMiddle(game)}
+                </div>
+
+                <div class="team_in_card">
+                <img src=${TeamsPage.photo_links[game.hTeam.triCode]} alt="">
+                <h3>${game.hTeam.triCode}</h3>
+                <p>${game.hTeam.win}-${game.hTeam.loss}</p>
+            </div>
             </a>
         `;
     }
