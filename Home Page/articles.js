34f045f43a964947f3005350acd665827847081a
dpipi17 <dpipi17@freeuni.edu.ge>
2020-12-21 23:59:03 +0400

    some quick updates

--- Home Page/articles.js
+++ Home Page/articles.js
@@ -5,14 +5,14 @@ export default class HomePage {
         return `
             <div class="articles">
                 <my-article imageSrc="https://cdn.nba.com/manage/2021/01/kawhi-laughs-high-fives-scaled.jpg" 
-                            title="Clippers jump Lakers for No. 1"
-                            text="See where all 30 teams rank after the first six weeks of the 2020-21 season..."
+                            title="Clippers for No. 1"
+                            text="See where all 30 teams rank after the first six weeks of..."
                             date="2 hours ago"></my-article>
                 
                 
                 <my-article imageSrc="https://cdn.nba.com/manage/2021/01/wiseman-intro-iso-scaled-e1611769078115.jpg" 
                             title="Kia Rookie Ladder"
-                            text="Several first-year players have impressed as the Rookie of the Year chase continues..."
+                            text="Several first-year players have impressed as the Rookie..."
                             date="8 hours ago"></my-article>
                             
                 
@@ -24,19 +24,19 @@ export default class HomePage {
                             
                 <my-article imageSrc="https://cdn.nba.com/manage/2021/01/GettyImages-1230268858-scaled-e1611766343523.jpg" 
                             title="La battle"
-                            text="When the LA Clippers and Los Angeles Lakers opened the 2019-20 season last October..."
+                            text="When the LA Clippers and Los Angeles Lakers opened..."
                             date="5 days ago"></my-article>
                             
                         
                 <my-article imageSrc="https://cdn.nba.com/manage/2021/01/lillard-winner.jpg" 
                             title="Dame time"
-                            text="Dame Lillard Hit 2 Crazy Threes In Final 10 Seconds To Beat Bulls..."
+                            text="Dame Lillard Hit 2 Crazy Threes In Final 10 Seconds..."
                             date="6 days ago"></my-article>
                             
                             
                 <my-article imageSrc="https://cdn.nba.com/manage/2020/12/20201230_gt_herro.mp4-1609375233286.png" 
                             title="Herro may miss"
-                            text="Miami Heat second-year guard Tyler Herro revealed that someone who lives with him tested..."
+                            text="Miami Heat second-year guard Tyler Herro revealed..."
                             date="6 days ago"></my-article>
                             
                         
@@ -47,8 +47,8 @@ export default class HomePage {
                             
                 
                 <my-article imageSrc="https://cdn.nba.com/manage/2021/01/nba-ball-general-view-iso.jpg" 
-                            title="NBA game postponements"
-                            text="The NBA's COVID-19 protocols have resulted in multiple games being postponed..."
+                            title="NBA games"
+                            text="The NBA's COVID-19 protocols have resulted in multiple..."
                             date="7 days ago"></my-article>
             </div>
         `
