e29f74c230289853811e0aec0b8224479c8b2461
dpipi17 <dpipi17@freeuni.edu.ge>
2021-01-15 22:45:10 +0400

    implement top part of player layout

--- Player Page/player.js
+++ Player Page/player.js
@@ -13,24 +13,51 @@ export default class PlayerPage {
         var teamsInfo = fetch('data/teams.json')
             .then(response => response.json());
 
-        var playerProfile = fetch("https://data.nba.net/prod/v1/2020/players/" + playerId + "_profile.json", {
+        var playerStats = fetch("https://data.nba.net/prod/v1/2020/players/" + playerId + "_profile.json", {
                 "method": "GET"
             })
             .then(response => response.json())
 
-        Promise.all([teamsInfo, playerProfile]).then((values) => {
-            callback(values[0].teamsInfo, values[1].league.standard);
+        var players = fetch('data/players.json')
+            .then(response => response.json());
+
+        Promise.all([teamsInfo, playerStats, players]).then((values) => {
+            callback(values[0].teamsInfo, values[1].league.standard, values[2]);
         });
     }
 
+    static renderTopPart(team, playerProfile, playerStats) {
+        return `
+            <div class="profile_container">
+                <div class="imgs_container">
+                    <img class="team_logo" src=" https://cdn.nba.com/logos/nba/${playerProfile.teamId}/global/L/logo.svg" alt=""></img>
+                    <img class="profile_img" src="https://cdn.nba.com/headshots/nba/latest/1040x760/${playerProfile.personId}.png" alt=""></img>
+                </div>
+
+                <div class="profile_info_container"> 
+                    <p>${team.displayName} | #${playerProfile.jersey} | ${playerProfile.teamSitesOnly.posFull}</p>
+                    <div class="name">
+                        ${playerProfile.firstName}
+                        <br>
+                        ${playerProfile.lastName}
+                    </div>
+                    <button class="btn">Compare</button>
+                </div>
+            </div>
+        `;
+    }
+
     static render(callback, playerId) {
-        this.loadData((teamsInfo, playerProfile) => {
-            var team = this.getTeamWithTeamId(teamsInfo, playerProfile.teamId);
-            console.log(this.getTeamWithTeamId(teamsInfo, playerProfile.teamId));
+        this.loadData((teamsInfo, playerStats, players) => {
+            var team = this.getTeamWithTeamId(teamsInfo, playerStats.teamId);
+            var playerProfile = players.find(player => player.personId == playerId);
+            console.log(playerProfile);
+            console.log(team);
             let content = `
-                <div class="player" style="background-color: ${team.primaryColor};">
-                    <img src="https://cdn.nba.com/headshots/nba/latest/1040x760/${playerId}.png" alt=""></img>
-                    <img src=" https://cdn.nba.com/logos/nba/${playerProfile.teamId}/global/L/logo.svg" alt=""></img>
+                <div class="player">
+                    <div class="top_part" style="background-color: ${team.primaryColor};">
+                        ${this.renderTopPart(team, playerProfile, playerStats)}
+                    </div>
                 </div>
             `;
 
