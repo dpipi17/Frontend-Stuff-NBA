80328b16237046fb95b5a284f2e36bb87c1fba97
dpipi17 <dpipi17@freeuni.edu.ge>
2021-01-16 18:49:37 +0400

    implement stats part of player profile (needs update to be responsive)

--- Player Page/player.js
+++ Player Page/player.js
@@ -44,6 +44,79 @@ export default class PlayerPage {
                     <button class="btn">Compare</button>
                 </div>
             </div>
+
+            <div class="top_stats">
+                <div class="main_stats">
+                    <div class="main_stat">
+                        MPG
+                        <br>
+                        ${playerStats.latest.mpg}
+                    </div>
+                    <div class="main_stat">
+                        PPG
+                        <br>
+                        ${playerStats.latest.ppg}
+                    </div>
+                    <div class="main_stat">
+                        RPG
+                        <br>
+                        ${playerStats.latest.rpg}
+                    </div>
+                    <div class="main_stat">
+                        APG
+                        <br>
+                        ${playerStats.latest.apg}
+                    </div>
+                    <div class="main_stat">
+                        SPG
+                        <br>
+                        ${playerStats.latest.spg}
+                    </div>
+                    <div class="main_stat last_main_stat">
+                        BPG
+                        <br>
+                        ${playerStats.latest.bpg}
+                    </div>
+                </div>
+
+                <div class="infos">                    
+                    <div class="info">
+                        HEIGHT 
+                        <br>
+                        ${playerProfile.heightFeet}'${playerProfile.heightInches}" (${playerProfile.heightMeters}m)
+                    </div>
+                    <div class="info">
+                        WEIGHT 
+                        <br>
+                        ${playerProfile.weightPounds}lb (${playerProfile.weightKilograms}kg)
+                    </div>
+                    <div class="info">
+                        COUNTRY 
+                        <br>
+                        ${playerProfile.country}
+                    </div>
+                    <div class="info">
+                        LAST ATTENDED
+                        <br>
+                        ${playerProfile.lastAffiliation}
+                    </div>
+                    <div class="info">
+                        College 
+                        <br>
+                        ${playerProfile.collegeName}
+                    </div>
+                    <div class="info">
+                        DRAFT 
+                        <br>
+                        ${playerProfile.draft.seasonYear} R${playerProfile.draft.roundNum} Pick ${playerProfile.draft.pickNum}
+                    </div>
+                    <div class="info last_info">
+                        EXPERIENCE 
+                        <br>
+                        ${playerProfile.yearsPro} Years
+                    </div>            
+                </div>
+            </div>
         `;
     }
 
@@ -53,10 +126,11 @@ export default class PlayerPage {
             var playerProfile = players.find(player => player.personId == playerId);
             console.log(playerProfile);
             console.log(team);
+            console.log(playerStats);
             let content = `
                 <div class="player">
                     <div class="top_part" style="background-color: ${team.primaryColor};">
-                        ${this.renderTopPart(team, playerProfile, playerStats)}
+                        ${this.renderTopPart(team, playerProfile, playerStats.stats)}
                     </div>
                 </div>
             `;
