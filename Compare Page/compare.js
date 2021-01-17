859c807630e6d95516f3c38fe4bb8010246bdc9c
dpipi17 <dpipi17@freeuni.edu.ge>
2021-01-17 23:12:03 +0400

    working on players compare

--- Compare Page/compare.js
+++ Compare Page/compare.js
@@ -15,17 +15,27 @@ export default class ComparePage {
         this.loadData((players) => {
             let content = `
                 <div class="compare">
-                    <my-players-search-bar id="left_search_bar"
-                                           players="${encodeURIComponent(JSON.stringify(players))}"
-                                           alignItems="flex-start"
-                                           placeHolder="Search first player...">
-                    </my-players-search-bar>
+                    <div class="compare_top_part">
+                        <div class="chosen_player">
+                            <img src="https://w7.pngwing.com/pngs/841/727/png-transparent-computer-icons-user-profile-synonyms-and-antonyms-android-android-computer-wallpaper-monochrome-sphere.png"></img>
+                            <my-players-search-bar id="left_search_bar"
+                                                players="${encodeURIComponent(JSON.stringify(players))}"
+                                                alignItems="flex-start"
+                                                placeHolder="Search first player...">
+                            </my-players-search-bar>
+                        </div>
 
-                    <my-players-search-bar id="right_search_bar"
-                                        players="${encodeURIComponent(JSON.stringify(players))}"
-                                        alignItems="flex-end"
-                                        placeHolder="Search second player...">
-                    </my-players-search-bar>
+                        <img class="vs" src="https://www.pngkey.com/png/full/20-208774_torrent-vs-magnet-link-graphic-design.png"></img>
+
+                        <div class="chosen_player">
+                            <img src="https://w7.pngwing.com/pngs/841/727/png-transparent-computer-icons-user-profile-synonyms-and-antonyms-android-android-computer-wallpaper-monochrome-sphere.png"></img>
+                            <my-players-search-bar id="right_search_bar"
+                                                players="${encodeURIComponent(JSON.stringify(players))}"
+                                                alignItems="flex-end"
+                                                placeHolder="Search second player...">
+                            </my-players-search-bar>
+                        </div>
+                    </div>
 
                 </div>
             `;
